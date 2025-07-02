9bb60e379a70b4becb843327c1e11b54cacc1efc
AjeeshViswambharan <n2nychannel@gmail.com>
2025-07-02 21:13:51 +0530

    first program

---
+++ testNN.py
@@ -0,0 +1,12 @@
+# Simple Python program to find the sum of two numbers
+
+# Take input from the user
+NIvedh = int(input("Enter the n: "))
+Nivedhita = int(input("Enter the second "))
+
+# Calculate the sum
+ajeesh = NIvedh + Nivedhita
+
+# Display the result
+print("The sum is:",ajeesh)
+#print("HAI")
